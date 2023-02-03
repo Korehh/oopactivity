873571277bc94e8b360605d32b5bdf57eb0040b4
Alprince Nacion <nacionalprince@gmail.com>
2023-02-02 20:13:24 -1200

    cart is updated and done

--- cart.php
+++ cart.php
@@ -15,13 +15,13 @@
                 echo 
                 '
                 <ul>
-                    <li>Name: ' . $item->getName() . '</li>
-                    <li>Description: ' . $item->getDescription() . '</li>
-                    <li>Price: ₱ ' . number_format($item->getPrice(), 1). '</li>
-                    <li>Dose: ' . $item->getDose() . '</li>
-                    <li>Type: ' . $item->getType() . '</li>
-                    <li>Exp Date: ' . $item->getExpirationDate() . '</li>
-                    <li>SRP: ' . $item->computeSRP() . '</li>
+                    <li><b>Name:</b> ' . $item->getName() . '</li>
+                    <li><b>Description:</b> ' . $item->getDescription() . '</li>
+                    <li><b>Price: ₱</b> ' . number_format($item->getPrice(), 1). '</li>
+                    <li><b>Dose:</b> ' . $item->getDose(). ' mg.' . '</li>
+                    <li><b>Type:</b> ' . $item->getType() . '</li>
+                    <li><b>Exp Date:</b> ' . $item->getExpirationDate() . '</li>
+                    <li><b>SRP:</b>  ' . number_format($item->computeSRP(), 2) . '</li>
                 </ul>
                 <hr>';
             }
